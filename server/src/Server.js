0647b11c12849d14166e7714e8400af2732d67a2
Sarah Baird <sarahjanebaird@gmail.com>
2022-01-24 11:26:07 -0400

    server setup

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,2 +1,24 @@
-// Express.js Server Code goes here
-// ...
+let express = require("express");
+let cors = require('cors');
+let path = require('path');
+let MongoClient = require("mongodb").MongoClient;
+let sanitizer = require('express-sanitizer');
+let ObjectId = require('mongodb').ObjectId;
+
+// MongoDB constants
+const URL = "mongodb://mongo:27017/";
+const DB_NAME = "dbTechs";
+
+// construct application object via express
+let app = express();
+// add cors as middleware to handle CORs errors while developing
+app.use(cors());
+
+// add middleware to work with incoming JSON
+app.use(express.json());
+app.use(sanitizer());
+
+// get absolute path to /build folder (production build of react web app)
+const CLIENT_BUILD_PATH = path.join(__dirname, "./../../client/build");
+// adding middleware to define static files location
+app.use("/", express.static(CLIENT_BUILD_PATH));
